882ab648c02a8b4cc7b3ea58f5aa21661b790860
Weißer Hase <wei3erHase@protonmail.com>
2023-02-07 16:49:18 +0100

    feat: eth mainnet deployment
    * feat: eth mainnet deployment
    
    * feat: mainnet addresses
    
    * feat: mainnet v2 addresses
    
    * fix: goerli addresses
    
    * chore: linter
    
    * fix: goerli addresses

--- eth-sdk/config.ts
+++ eth-sdk/config.ts
@@ -3,8 +3,12 @@ import {defineConfig} from '@dethcrypto/eth-sdk';
 export default defineConfig({
   contracts: {
     goerli: {
-      dataFeedJob: '0x606e25c67B8d6550075C8085083c763769Cfe2BE',
-      dataFeed: '0x8Fb68E83831c7e8622e10C154CC0d24856440809',
+      dataFeedJob: '0x6c461C0296eBE3715820F1Cbde856219e06ac3B8',
+      dataFeed: '0x553365bdda2Fd60608Fb05CB7ad32620e3A126DD',
+    },
+    mainnet: {
+      dataFeedJob: '0x1f5f0DA9391AB08c7F0150d45B41F6900fb4Fd0C',
+      dataFeed: '0x1ce81290Eb4c10cC9Fa71256799665423e87b628',
     },
   },
 });
